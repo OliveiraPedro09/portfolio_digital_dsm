0cb898001d7fc9e96af8a27415c7739f60b0d02c
Pedro Martins de Oliveira <142221456+OliveiraPedro09@users.noreply.github.com>
2024-06-11 10:24:32 -0300

    Atualização dos Trabalhos do Portifólio
    Acrescentado o trabalho do segundo semestre DSM

--- api/app.py
+++ api/app.py
@@ -16,10 +16,10 @@ def sobre():
 @app.route('/trabalhos')
 def trabalhos():
     trabalho = [
-        {'titulo' : 'TRABALHO ACADÊMICO API FATEC-SJC', 'conteudo' : 'Metodologia', 'paragrafo' : 'SCRUM - O que é o Scrum? O Scrum é um framework de gerenciamento que as equipes usam para se auto-organizar e trabalhar em direção a um objetivo em comum. A estrutura descreve um conjunto de reuniões, ferramentas e funções para uma entrega eficiente de projetos.' },
+        {'titulo' : 'TRABALHO ACADÊMICO API FATEC-SJC 1º DSM', 'conteudo' : 'Metodologia', 'paragrafo' : 'SCRUM - O que é o Scrum? O Scrum é um framework de gerenciamento que as equipes usam para se auto-organizar e trabalhar em direção a um objetivo em comum. A estrutura descreve um conjunto de reuniões, ferramentas e funções para uma entrega eficiente de projetos.' },
         {'titulo1' : 'Sobre o Projeto', 'conteudo1' : 'Estou atualmente desenvolvendo com a minha equipe Code Nine um projeto onde devemos fazer um site informativo sobre nefrologia infantil, um assunto que é pouco abordado na sociedade brasileira.', 'paragrafo1' : 'Esse projeto foi desenvolvido com a finalidade de conscientizar, ampliar e facilitar a comunicação de pacientes e famílias que possuem Doença renal crônica(DRC).', },
-        {'titulo2' : 'Oque é DRC ?', 'conteudo2': 'É uma doença de curso pronlongado, que pode parecer benigno, porém, na maioria das vezes se torna grave, além de ser uma doença silenciosa.'},
-        {'titulo3' : 'Fatores de Risco:', 'conteudo3': 'Um dos principais fatores de risco para doença renal crônica é a diabetes e a hipertensão, ambas cuidadas na Atenção Básica, principal porta de entrada para o Sistema Único de Saúde (SUS), em uma das 42.885 Unidades Básicas de Saúde distribuídas em todo o Brasil.'}
+        {'titulo2' : 'TRABALHO ACADÊMICO API FATEC-SJC 2º DSM', 'conteudo2': 'Desenvolvimento de um sistema de atendimento direcionado a uma empresa de transportes e fretes'},
+        {'titulo3' : 'Sobre o Projeto', 'conteudo3': 'O projeto foi executando usando React Typescript, visando criar um site para atendimentos de uma empresa de entregas conectando o cliente, atendente e empresa. Possui um sitema de chat e tickets onde o atendente consegue se conectar de forma eficiênte ao cliente.'}
         ]
     return render_template('trabalhos.html', trabalho = trabalho)
 
